de7e0f3c0ace5b2bbe70878e5240987188954d58
Artem <artem.bqpower@gmail.com>
2024-01-30 21:26:55 +0300

    change env

--- src/components/providers/convex-provider.tsx
+++ src/components/providers/convex-provider.tsx
@@ -5,7 +5,7 @@ import { ConvexReactClient } from 'convex/react'
 import { ConvexProviderWithClerk } from 'convex/react-clerk'
 import { ClerkProvider, useAuth } from '@clerk/clerk-react'
 
-const convex = new ConvexReactClient('https://capable-husky-926.convex.cloud');
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const ConvexClientProvider = ({
     children
